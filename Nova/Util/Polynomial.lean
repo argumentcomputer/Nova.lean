c81e3eb852453236f5b76b29f7d54498889033f6
DanielRrr <daniel.rogozin@serokell.io>
2022-09-27 14:39:10 +0300

    add polynomial division

--- Nova/Util/Polynomial.lean
+++ Nova/Util/Polynomial.lean
@@ -7,6 +7,11 @@ def Polynomial R := Array R
 instance : Append (Polynomial A) where
   append := Array.append
 
+def tail (ar : Polynomial A) : Polynomial A :=
+  match ar.data with
+    | [] => Array.empty
+    | (_ :: xs) => Array.mk xs
+
 variable {A : Type} [Add A] [Mul A] [HPow A Nat A] [OfNat A 1] [ofnat : OfNat A 0] 
                     [hab : Inhabited A] [eq : BEq A] [Div A] [Neg A]
 
@@ -25,7 +30,7 @@ def isZero (f : Polynomial A) : Bool :=
 
 def isMonic (f : Polynomial A) : Bool :=
   let f' := norm f
-  if f'[f'.size-1] == 1 then true else false
+  if f'[0] == 1 then true else false
 
 def lead (f : Polynomial A) : A := Array.getD f 0 0
 
@@ -49,7 +54,7 @@ def zero : Polynomial A := #[0]
 def shift (f : Polynomial A) (n : Nat) : Polynomial A :=
   f ++ zeros n
 
-def polAdd (f : Polynomial A) (g : Polynomial A) : Polynomial A :=
+def polyAdd (f : Polynomial A) (g : Polynomial A) : Polynomial A :=
   let action (f₁ : Polynomial A) (f₂ : Polynomial A) := Array.map (fun (x, y) => x + y) (Array.zip f₁ f₂)
   if f.size < g.size
   then
@@ -60,33 +65,44 @@ def polAdd (f : Polynomial A) (g : Polynomial A) : Polynomial A :=
     else action f g
 
 instance : Add (Polynomial A) where
-  add := polAdd
+  add := polyAdd
 
 def polySub (f : Polynomial A) (g : Polynomial A) : Polynomial A :=
-  polAdd f (Array.map Neg.neg g)
+  polyAdd f (Array.map Neg.neg g)
 
-def polMul (f : Polynomial A) : Polynomial A → Polynomial A :=
-  Array.foldr (fun x acc => polAdd (mulByConst x f) (zero ++ acc)) (#[] : Polynomial A)
+def polyMul (f : Polynomial A) : Polynomial A → Polynomial A :=
+  Array.foldr (fun x acc => polyAdd (mulByConst x f) (zero ++ acc)) (#[] : Polynomial A)
 
 instance : Mul (Polynomial A) where
-  mul := polMul
+  mul := polyMul
 
 instance : Sub (Polynomial A) where
   sub := polySub
 
-def polDiv (f : Polynomial A) (g : Polynomial A) : 
-    Polynomial A × Polynomial A :=
-  let f' := norm f
-  let g' := norm g
-  let rec helper (x : Polynomial A) (y : Polynomial A) (n : Nat) : Polynomial A × Polynomial A :=
+def polyDiv (f : Polynomial A) (g : Polynomial A) : Polynomial A :=
+  let rec polDiv' f' g' (n : Nat) :=
     match n with
-      | .zero => (x, y)
-      | .succ k => 
-        let t : Polynomial A := #[lead x / lead y]
-        let y' := norm (y + t)
-        let x' := norm (x - t * g')
-        helper (norm x') (norm y') k
-  let (f'', g'') := helper f' g' (degree f')
-  (norm f'', norm g'')
+      | 0 => Array.empty
+      | k + 1 =>
+        if k + 1 < g'.size then Array.empty
+        else
+          let x := Array.getD f' 0 0
+          let y := Array.getD g' 0 0
+          let z := x / y
+          #[z] ++ polDiv' (tail (polySub f' (polyMul #[z] g'))) g' k 
+  Array.reverse $ polDiv' (Array.reverse f) (Array.reverse g) (f.size)
+
+def polyMod (f : Polynomial A) (g : Polynomial A) : Polynomial A :=
+    let rec polyMod' f' g' (n : Nat) :=
+    match n with
+      | 0 => Array.empty
+      | k + 1 =>
+        if k + 1 < g'.size then Array.empty
+        else
+          let x := Array.getD f' 0 0
+          let y := Array.getD g' 0 0
+          let z := x / y
+          polyMod' (tail (polySub f' (polyMul #[z] g'))) g' k 
+  Array.reverse $ polyMod' (Array.reverse f) (Array.reverse g) (f.size)
 
 end Polynomial
