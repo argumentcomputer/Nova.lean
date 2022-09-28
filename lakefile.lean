d9ffc9075c0d698cdb112249597a6c32085b2d56
DanielRrr <daniel.rogozin@serokell.io>
2022-09-28 15:28:52 +0300

    bump the lean4 version and add YatimaStdLib

--- lakefile.lean
+++ lakefile.lean
@@ -1,6 +1,7 @@
 import Lake
 open Lake DSL
 
-package Nova.lean {
-  -- add configuration options here
-}
+package Nova
+
+require YatimaStdLib from git
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "e36549ca02f3389b997ea01e7e9781abdef46ae3"
