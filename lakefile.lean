fcb97cc9381435810028dd2d5d0523ba8b3de21f
DanielRrr <daniel.rogozin@serokell.io>
2022-10-07 19:39:08 +0300

    get rid of Polynomial.lean in favour of its counterpart in YatimaStdLib

--- lakefile.lean
+++ lakefile.lean
@@ -7,4 +7,4 @@ package Nova
 lean_lib Nova
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "868abcb58c5d96927c65ac304ee1e99a21d57457"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "3cb5f4706cd49c1803cdb7a5aec53ca58e8a8b1a"
