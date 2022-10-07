551727a9b254dd3547236e548db230e5325b4359
DanielRrr <daniel.rogozin@serokell.io>
2022-10-07 18:14:02 +0300

    define roots of unity

--- Nova/Util/GaloisField.lean
+++ Nova/Util/GaloisField.lean
@@ -1,16 +1,30 @@
 import YatimaStdLib.Zmod
 
+/-
+Here we post some definitions from https://hackage.haskell.org/package/galois-field-1.0.2
+-/
+
 namespace GaloisField
 
 class GaloisField (K : Type _) 
       [Add K] [Mul K] [Sub K] [Div K] where
+  -- Characteristic @p@ of field and order of prime subfield.
   char : Nat
-  deg : K → Nat
+
+  -- Degree q of field as extension field over prime subfield.
+  deg : Nat
+
+  -- Frobenius endomorphism x ↦ x^p of prime subfield.
   frob : K → K
+
+variable {K : Type} [Add K] [Mul K] [Sub K] [Div K] [gal : GaloisField K]
+
+-- Order p^q of field.
+def order : Nat := gal.char ^ gal.deg
 
 instance : GaloisField (Zmod p) where
   char := p - 1
-  deg := fun _ => 1
+  deg := 1
   frob r := r ^ p
 
 class PrimeField (K : Type _) 
---
+++ Nova/Util/RootsOfUnity.lean
@@ -0,0 +1,45 @@
+import Nova.Util.GaloisField
+
+namespace RootsOfUnity
+
+-- n-th roots of unity of Galois fields
+inductive RootsOfUnity (n : Nat) (A : Type _) where
+  | U : A → RootsOfUnity n A
+
+-- Group operations on roots of unity
+instance [Mul A] : Mul (RootsOfUnity n A) where
+  mul a b :=
+    match (a, b) with
+    | (.U x, .U y) => .U $ x * y
+
+instance [OfNat A 1] : OfNat (RootsOfUnity n A) 1 where
+  ofNat := .U 1
+
+instance [Div A] : Div (RootsOfUnity n A) where
+  div a b :=
+    match (a, b) with
+    | (.U x, .U y) => .U $ x / y
+
+open GaloisField
+
+variable {K : Type} [add : Add K] [mul : Mul K] 
+         [sub : Sub K] [div : Div K] [gal : GaloisField K]
+         [HPow K Nat K] [BEq K] [OfNat K 1]
+
+-- Cardinality of the subgroup
+def cardinality : RootsOfUnity n K → Nat := fun _ => n
+
+-- Cofactor of the subgroup in the group.
+def cofactor : RootsOfUnity n K → Nat := 
+  let ord := @order K add mul sub div gal
+  (fun x => ord / x) ∘ cardinality
+
+-- isUnity checks is a given element of a Galois field is unity
+def isUnity (k : K) (n : Nat) : Bool :=
+  k ^ n == 1
+
+def isRootOfUnity (r : RootsOfUnity n K) : Bool :=
+  match r with
+    | .U x => isUnity x $ cardinality r
+
+end RootsOfUnity
