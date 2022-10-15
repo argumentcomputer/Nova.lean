d5984bb0618dfc42a8f36d9d0adeb22a5be8a1f8
DanielRrr <daniel.rogozin@serokell.io>
2022-10-15 14:37:01 +0300

    add the ECPairing type class

--- Nova/Util/Pairing/Pairing.lean
+++ Nova/Util/Pairing/Pairing.lean
@@ -10,26 +10,6 @@ class Pairing (G₁ : Type _) (G₂ : Type _) (Gₜ : Type _) where
 
 open EllipticCurves Weierstrass AffineCurves
 
-class ECPairingG₁ (q : Nat) (r : Nat) 
-  [Curve (Zmod q) (Zmod r)]
-  [WCurve (Zmod q) (Zmod r)]
-  [WACurve (Zmod q) (Zmod r)]
-
-open GaloisField
-
-class ECPairingG₂ (q : Nat) (r : Nat) (U : Type _)
-  [IrreducibleMonic U (Zmod q)]
-  [Curve (Extension U (Zmod q)) (Zmod r)]
-  [WCurve (Extension U (Zmod q)) (Zmod r)]
-  [WACurve (Extension U (Zmod q)) (Zmod r)]
-
-class ECPairingGT (q : Nat) (U : Type _) (V : Type _) (W : Type _)
-  [GaloisField (Extension U (Zmod q))]
-  [GaloisField (Extension V (Extension U (Zmod q)))]
-  [IrreducibleMonic V (Extension U (Zmod q))]
-  [IrreducibleMonic W (Extension V (Extension U (Zmod q)))]
-
-/-
 class ECPairing (q : Nat) (r : Nat) (U : Type _) (V : Type _) (W : Type _)
   [Curve (Zmod q) (Zmod r)]
   [GaloisField (Extension U (Zmod q))]
@@ -46,7 +26,6 @@ class ECPairing (q : Nat) (r : Nat) (U : Type _) (V : Type _) (W : Type _)
   [Curve (Extension U (Zmod q)) (Zmod r)]
   [WCurve (Extension U (Zmod q)) (Zmod r)]
   [WACurve (Extension U (Zmod q)) (Zmod r)]
--/
 
 open RootsOfUnity
 
