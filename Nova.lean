19d248b62e217bc5d1b97db432e8321777101116
DanielRrr <daniel.rogozin@serokell.io>
2022-10-20 14:17:33 +0300

    fix the file Nova.lean

--- Nova.lean
+++ Nova.lean
@@ -1,8 +1,4 @@
-import Nova.Util.BLS12381
-import Nova.Util.BLS12381T
 import Nova.Util.EllipticCurves
 import Nova.Util.GaloisField
 import Nova.Util.RootsOfUnity
-import Nova.Util.Pairing.Ate
-import Nova.Util.Pairing.BLS12381
 import Nova.Util.Pairing.Pairing
