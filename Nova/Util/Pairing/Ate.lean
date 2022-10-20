8f772c0186beed9015c832a29ec406ca3c25ac4d
DanielRrr <daniel.rogozin@serokell.io>
2022-10-20 14:02:51 +0300

    more fixes and simplifications

--- Nova/Util/Pairing/Ate.lean
+++ Nova/Util/Pairing/Ate.lean
@@ -7,19 +7,15 @@ namespace Ate
 
 variable {q r : Nat} {U V W : Type _}
 
-open EllipticCurves Weierstrass AffineCurves GaloisField RootsOfUnity Pairing Form Coordinate
+open EllipticCurves GaloisField RootsOfUnity Pairing
 
-variable [Curve Weierstrass Affine (AffinePoint (Zmod q) (Zmod r)) (Zmod q) (Zmod r)]
+variable [Curve (Zmod q) (Zmod r)]
 variable [GaloisField (Extension U (Zmod q))]
-variable [WCurve Affine (AffinePoint (Zmod q) (Zmod r)) (Zmod q) (Zmod r)]
-variable [WACurve (Zmod q) (Zmod r)]
-variable [cur : Curve Weierstrass Affine (AffinePoint (Extension U (Zmod q)) (Zmod r)) (Extension U (Zmod q)) (Zmod r)]
+variable [Curve (Extension U (Zmod q)) (Zmod r)]
 variable [IrreducibleMonic U (Zmod q)]
 variable [GaloisField (Extension V (Extension U (Zmod q)))]
 variable [IrreducibleMonic V (Extension U (Zmod q))]
 variable [IrreducibleMonic W (Extension V (Extension U (Zmod q)))]
-variable [WCurve Affine (AffinePoint (Extension U (Zmod q)) (Zmod r)) (Extension U (Zmod q)) (Zmod r)]
-variable [WACurve (Extension U (Zmod q)) (Zmod r)]
 variable [Pairing (AffinePoint (Zmod q) (Zmod r)) (AffinePoint (Extension U (Zmod q)) (Zmod r)) (GT U V W q r)]
 
 def millerLoop
@@ -34,7 +30,7 @@ def millerAlgorithmBLS12
   GT U V W q r :=
     match l with
       | x :: xs => 
-        (fun (x, y) => y) $ millerLoop a b xs (if x > 0 then b else cur.inv Weierstrass Affine (Extension U (Zmod q)) (Zmod r) b, _)
+        (fun (x, y) => y) $ millerLoop a b xs (if x > 0 then b else cur.inv (Extension U (Zmod q)) (Zmod r) b, _)
       | [] => .U $ .E #[.E #[.E #[1]]]
 
 end Ate
