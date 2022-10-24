d78660cbf41a6c1491cdd470b12391e9128bac8b
DanielRrr <daniel.rogozin@serokell.io>
2022-10-24 19:35:17 +0300

    define USize constraints

--- Nova/SNARK/Circuit.lean
+++ Nova/SNARK/Circuit.lean
@@ -1,8 +1,18 @@
 namespace Circuit
 
+/-
+class StepCircuit (F : Type _) where
+  arity : F → USize
+  synthesise : _
+  output : Array F
+-/
+
 structure NovaAugmentedCircuitParams where
   limb_width : USize
   n_limbs : USize
   is_primary_circuit : Bool
+
+structure TrivialTestCircuit (F : Type _) where
+  _p : F
 
 end Circuit
--- Nova/SNARK/PublicParams.lean
+++ Nova/SNARK/PublicParams.lean
@@ -1,5 +1,6 @@
 import Nova.SNARK.Circuit
 import Nova.SNARK.Commitments
+import Nova.SNARK.Constraints
 import Nova.SNARK.R1CS
 
 open Circuit Commitments R1CS
@@ -13,14 +14,14 @@ structure PublicParams
   (C₁ : Type _) (C₂ : Type _) where
   F_arity_primary : USize
   F_arity_secondary : USize
-  -- ro_consts_primary : ROConstants G₁
-  -- ro_consts_circuit_primary : ROConstantsCircuit G₂
+  --ro_consts_primary : ROConstants G₁
+  --ro_consts_circuit_primary : ROConstantsCircuit G₂
   
   r1cs_gens_primary : R1CSGens PreprocessedGroupElement G₁
   r1cs_shape_primary : R1CSShape G₁
   r1cs_shape_padded_primary : R1CSShape G₁
-  -- ro_consts_secondary : ROConstants G₂
-  -- ro_consts_circuit_secondary : ROConstantsCircuit G₁
+  --ro_consts_secondary : ROConstants G₂
+  --ro_consts_circuit_secondary : ROConstantsCircuit G₁
   r1cs_gens_secondary : R1CSGens PreprocessedGroupElement G₁
   r1cs_shape_secondary  : R1CSShape G₁
   r1cs_shape_padded_secondary : R1CSShape G₁
@@ -28,5 +29,11 @@ structure PublicParams
   augmented_circuit_params_secondary : NovaAugmentedCircuitParams
   _p_c1 : C₁
   _p_c2 : C₂
+
+def augmented_circuit_params_primary : NovaAugmentedCircuitParams :=
+  NovaAugmentedCircuitParams.mk BN_LIMB_WIDTH BN_N_LIMBS true
+
+def augmented_circuit_params_secondary : NovaAugmentedCircuitParams :=
+  NovaAugmentedCircuitParams.mk BN_LIMB_WIDTH BN_N_LIMBS true
 
 end PublicParams
