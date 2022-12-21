ddd36977286a26f578b7bf3596f7c4a3d58c3682
DanielRrr <daniel.rogozin@serokell.io>
2022-12-21 17:47:20 +0400

    get rid of GaloisField.lean

--- lakefile.lean
+++ lakefile.lean
@@ -7,4 +7,4 @@ package Nova
 lean_lib Nova
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "ea96c587a3f9e8f65719b6f4f70f1305c560c6ac"
+  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "7ef2fdec4176c6cd01cfd6891ca5ed07de2848d9"
